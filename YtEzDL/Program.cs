a80a654150dc691a63ddb3ba14445ca023cded80
Tim Horemans <tim.horemans@tenforce.com>
2018-04-11 22:00:36 +0200

    Allow only one instance

--- YtEzDL/Program.cs
+++ YtEzDL/Program.cs
@@ -1,16 +1,28 @@
 ﻿using System;
+using System.Reflection;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace YtEzDL
 {
     static class Program
     {
+        private static Mutex _mutex;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            var mutexName = Assembly.GetExecutingAssembly().FullName;
+
+            // Check if mutex already exists
+            if (Mutex.TryOpenExisting(mutexName, out _mutex))
+                return;
+
+            _mutex = new Mutex(true, mutexName);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new ApplicationContext());
