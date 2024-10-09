f148209e329dabc507d1303ce176d739a53c4dd6
Tim Horemans <tim.horemans@tenforce.com>
2024-10-09 14:08:02 +0200

    UI

--- YtEzDL/Forms/DownloadForm.Designer.cs
+++ YtEzDL/Forms/DownloadForm.Designer.cs
@@ -41,10 +41,10 @@ namespace YtEzDL.Forms
             this.metroButtonDownload.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.metroButtonDownload.Enabled = false;
             this.metroButtonDownload.FontSize = MetroFramework.MetroButtonSize.Medium;
-            this.metroButtonDownload.Location = new System.Drawing.Point(15, 451);
+            this.metroButtonDownload.Location = new System.Drawing.Point(11, 367);
             this.metroButtonDownload.Margin = new System.Windows.Forms.Padding(2);
             this.metroButtonDownload.Name = "metroButtonDownload";
-            this.metroButtonDownload.Size = new System.Drawing.Size(248, 34);
+            this.metroButtonDownload.Size = new System.Drawing.Size(186, 26);
             this.metroButtonDownload.Style = MetroFramework.MetroColorStyle.Blue;
             this.metroButtonDownload.TabIndex = 3;
             this.metroButtonDownload.Text = "Download";
@@ -55,10 +55,10 @@ namespace YtEzDL.Forms
             // 
             this.metroButtonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.metroButtonCancel.FontSize = MetroFramework.MetroButtonSize.Medium;
-            this.metroButtonCancel.Location = new System.Drawing.Point(288, 451);
+            this.metroButtonCancel.Location = new System.Drawing.Point(219, 367);
             this.metroButtonCancel.Margin = new System.Windows.Forms.Padding(2);
             this.metroButtonCancel.Name = "metroButtonCancel";
-            this.metroButtonCancel.Size = new System.Drawing.Size(248, 34);
+            this.metroButtonCancel.Size = new System.Drawing.Size(186, 26);
             this.metroButtonCancel.Style = MetroFramework.MetroColorStyle.Blue;
             this.metroButtonCancel.TabIndex = 4;
             this.metroButtonCancel.Text = "Cancel";
@@ -73,20 +73,21 @@ namespace YtEzDL.Forms
             this.flowLayoutPanel.AutoScroll = true;
             this.flowLayoutPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
             this.flowLayoutPanel.ImeMode = System.Windows.Forms.ImeMode.On;
-            this.flowLayoutPanel.Location = new System.Drawing.Point(15, 74);
+            this.flowLayoutPanel.Location = new System.Drawing.Point(11, 57);
+            this.flowLayoutPanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.flowLayoutPanel.Name = "flowLayoutPanel";
-            this.flowLayoutPanel.Size = new System.Drawing.Size(970, 351);
+            this.flowLayoutPanel.Size = new System.Drawing.Size(728, 294);
             this.flowLayoutPanel.TabIndex = 12;
             this.flowLayoutPanel.WrapContents = false;
             this.flowLayoutPanel.Resize += new System.EventHandler(this.flowLayoutPanel_Resize);
             // 
             // DownloadForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
-            this.ClientSize = new System.Drawing.Size(1000, 500);
+            this.ClientSize = new System.Drawing.Size(750, 405);
             this.Controls.Add(this.flowLayoutPanel);
             this.Controls.Add(this.metroButtonCancel);
             this.Controls.Add(this.metroButtonDownload);
@@ -94,7 +95,7 @@ namespace YtEzDL.Forms
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(1);
             this.Name = "DownloadForm";
-            this.Padding = new System.Windows.Forms.Padding(12, 60, 12, 13);
+            this.Padding = new System.Windows.Forms.Padding(9, 46, 9, 10);
             this.Text = "DownloadForm";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.DownloadForm_FormClosing);
             this.ResumeLayout(false);
