4c66f4ea3160f067b932c7403511eaf23c6e8d4c
Tim Horemans <tim.horemans@tenforce.com>
2024-10-10 21:44:24 +0200

    Added initial Settings form

---
+++ YtEzDL/Forms/Settings.cs
@@ -0,0 +1,12 @@
+﻿using MetroFramework.Forms;
+
+namespace YtEzDL.Forms
+{
+    public partial class Settings : MetroForm
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+    }
+}
