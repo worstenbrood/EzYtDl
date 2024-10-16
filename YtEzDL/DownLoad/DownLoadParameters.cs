f3ff49d3645e86a6a1fc08a83d7b9353456d97a7
Tim Horemans <tim.horemans@tenforce.com>
2024-10-16 14:56:13 +0200

    Added parameters

--- YtEzDL/DownLoad/DownLoadParameters.cs
+++ YtEzDL/DownLoad/DownLoadParameters.cs
@@ -44,90 +44,107 @@ namespace YtEzDL.DownLoad
     {
         public static DownLoadParameters Create => new DownLoadParameters();
 
+        private DownLoadParameters Add(string key, string value = null)
+        {
+            this[key] = value;
+            return this;
+        }
+
         public DownLoadParameters RemoveCache()
         {
-            this["--rm-cache-dir"] = string.Empty;
-            return this;
+            return Add("--rm-cache-dir");
         }
 
         public DownLoadParameters ExtractAudio()
         {
-            this["-x"] = string.Empty;
-            return this;
+            return Add("-x");
         }
 
         public DownLoadParameters AddMetadata()
         {
-            this["--add-metadata"] = string.Empty;
-            return this;
+            return Add("--add-metadata");
         }
 
         public DownLoadParameters EmbedThumbnail()
         {
-            this["--embed-thumbnail"] = string.Empty;
-            return this;
+            return Add("--embed-thumbnail");
         }
 
         public DownLoadParameters AudioFormat(AudioFormat format)
         {
-            this["--audio-format"] = format.ToString("G").ToLowerInvariant();
-            return this;
+            return Add("--audio-format", format.ToString("G").ToLowerInvariant());
         }
 
         public DownLoadParameters AudioQuality(AudioQuality quality)
         {
-            this["--audio-quality"] = quality.ToString("D");
-            return this;
+            return Add("--audio-quality", quality.ToString("D"));
         }
 
         public DownLoadParameters MetadataFromTitle(string format)
         {
-            this["--metadata-from-title"] = format;
-            return this;
+            return Add("--metadata-from-title", format);
         }
 
         public DownLoadParameters VideoFormat(VideoFormat format)
         {
-            this["--recode-video"] = format.ToString("G").ToLowerInvariant();
-            return this;
+            return Add("--recode-video", format.ToString("G").ToLowerInvariant());
         }
 
         public DownLoadParameters IgnoreErrors()
         {
-            this["--ignore-errors"] = string.Empty;
-            return this;
+            return Add("--ignore-errors");
         }
 
         public DownLoadParameters SetPath(string path)
         {
-            this["-P"] = $"\"{path}\"";
-            return this;
+            return Add("-P", $"\"{path}\"");
         }
 
         public DownLoadParameters GetJson()
         {
-            this["-j"] = string.Empty;
-            return this;
+            return Add("-j");
         }
 
         public DownLoadParameters Update()
         {
-            this["--update"] = string.Empty;
-            return this;
+            return Add("--update");
         }
 
         public DownLoadParameters Version()
         {
-            this["--version"] = string.Empty;
-            return this;
+            return Add("--version");
         }
 
         internal DownLoadParameters Url(string url)
         {
-            this[$"\"{url}\""] = string.Empty;
-            return this;
+            return Add($"\"{url}\"");
         }
-        
+
+        public DownLoadParameters FlatPlaylist()
+        {
+            return Add("--flat-playlist");
+        }
+
+        public DownLoadParameters LazyPlayList()
+        {
+            return Add("--lazy-playlist");
+        }
+
+        public DownLoadParameters NoCleanInfoJson()
+        {
+            return Add("--no-clean-info-json");
+        }
+
+        public DownLoadParameters ParseMetadata(string expression)
+        {
+            return Add("--parse-metadata", expression);
+        }
+
+        public DownLoadParameters ReplaceMetadata(string expression)
+        {
+            return Add("--replace-in-metadata", expression);
+        }
+
         public DownLoadParameters Reset()
         {
             Clear();
