bc9f97f22dfcc5ee5964ed135b0ad9611235cfae
Tim Horemans <tim.horemans@tenforce.com>
2024-10-17 21:32:23 +0200

    Cleanup

--- YtEzDL/Forms/About.cs
+++ YtEzDL/Forms/About.cs
@@ -2,7 +2,6 @@
 using System.Diagnostics;
 using System.Drawing;
 using System.Reflection;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using MetroFramework;
 using MetroFramework.Forms;
