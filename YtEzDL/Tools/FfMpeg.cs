07091c4c51de3e3823775c12d3376e2cf10d6c56
Tim Horemans <tim.horemans@tenforce.com>
2024-10-29 17:30:23 +0100

    Expose stream

--- YtEzDL/Tools/FfMpeg.cs
+++ YtEzDL/Tools/FfMpeg.cs
@@ -8,7 +8,7 @@ using YtEzDL.Utils;
 
 namespace YtEzDL.Tools
 {
-    public delegate void DataOutput(byte[] data, int length);
+    public delegate bool DataReader(BinaryReader reader);
 
     public class FfMpeg : ConsoleProcess, ITool
     {
@@ -30,22 +30,18 @@ namespace YtEzDL.Tools
 
         private static readonly YoutubeDownload YoutubeDownload = new YoutubeDownload();
 
-        private static void OutputReader(Process process, DataOutput output, CancellationToken cancellationToken)
+        private static void OutputReader(Process process, DataReader reader, CancellationToken cancellationToken)
         {
             var binaryReader = new BinaryReader(process.StandardOutput.BaseStream);
-            var buffer = new byte[DefaultBufferSize];
-
+            
             try
             {
-                int bytesRead;
-                while ((bytesRead = binaryReader.Read(buffer, 0, DefaultBufferSize)) > 0)
+                while (reader(binaryReader))
                 {
                     if (cancellationToken.IsCancellationRequested)
                     {
                         break;
                     }
-
-                    output?.Invoke(buffer, bytesRead);
                 }
             }
             finally
@@ -62,7 +58,7 @@ namespace YtEzDL.Tools
         /// <param name="format">AudioFormat</param>
         /// <param name="cancellationToken">Cancellation token</param>
         /// <returns></returns>
-        public async Task ConvertToAudio(string url, DataOutput output, AudioFormat format = AudioFormat.S16Le,
+        public async Task ConvertToAudio(string url, DataReader output, AudioFormat format = AudioFormat.S16Le,
             CancellationToken cancellationToken = default)
         {
             var parameters = new[]
@@ -88,10 +84,21 @@ namespace YtEzDL.Tools
             await Task.WhenAll(reader, writer);
         }
 
+        private static bool Redirect(BinaryReader reader, byte[] buffer, int length, Stream output)
+        {
+            var bytesRead = reader.Read(buffer, 0, length);
+            if (bytesRead > 0)
+            {
+                output.Write(buffer, 0, bytesRead);
+            }
+            return bytesRead != 0;
+        }
+
         public async Task ConvertToAudio(string url, Stream output, AudioFormat format = AudioFormat.S16Le,
             CancellationToken cancellationToken = default)
         {
-            await ConvertToAudio(url, (b, c) => output.Write(b, 0, c), format, cancellationToken);
+            var buffer = new byte[DefaultBufferSize];
+            await ConvertToAudio(url, s => Redirect(s, buffer, DefaultBufferSize, output), format, cancellationToken);
         }
     }
 }
