ba648c917416a3c419a7a584f097413fff3378f7
Tim Horemans <tim.horemans@tenforce.com>
2024-11-12 21:54:48 +0100

    Made waveout public

--- YtEzDL/Utils/AudioPlayer.cs
+++ YtEzDL/Utils/AudioPlayer.cs
@@ -8,42 +8,42 @@ namespace YtEzDL.Utils
     public class AudioPlayer : IDisposable
     {
         private static readonly WaveFormat Format = new WaveFormat(48000, 16, 2);
-        private readonly WaveOut _waveOut;
+        public readonly WaveOut WaveOut;
         private readonly FfMpegStream _ffMpegStream;
 
-        public AudioPlayer(string url)
+        public AudioPlayer(string url, int desiredLatency = 60, int numberOfBuffers = 10)
         {
             // Init device
-            _waveOut = new WaveOut(WaveCallbackInfo.FunctionCallback());
-            _waveOut.DesiredLatency = 60;
-            _waveOut.NumberOfBuffers = 10;
-            _waveOut.PlaybackStopped += (sender, e) =>
+            WaveOut = new WaveOut(WaveCallbackInfo.FunctionCallback());
+            WaveOut.DesiredLatency = desiredLatency;
+            WaveOut.NumberOfBuffers = numberOfBuffers;
+            WaveOut.PlaybackStopped += (sender, e) =>
             {
-                _waveOut.Stop();
+                WaveOut.Stop();
             };
 
             // Create ffmpeg stream
             _ffMpegStream = new FfMpegStream(url, AudioFormat.Wav);
 
             // Init stream
-            _waveOut.Init(new RawSourceWaveStream(_ffMpegStream, Format));
+            WaveOut.Init(new RawSourceWaveStream(_ffMpegStream, Format));
         }
 
-        public void Play() => _waveOut.Play();
-        public void Pause() => _waveOut.Pause();
-        public void Resume() => _waveOut.Resume();
-        public void Stop() => _waveOut.Stop();
-        public long Position => _waveOut.GetPosition();
-        public PlaybackState PlaybackState => _waveOut.PlaybackState;
+        public void Play() => WaveOut.Play();
+        public void Pause() => WaveOut.Pause();
+        public void Resume() => WaveOut.Resume();
+        public void Stop() => WaveOut.Stop();
+        public long Position => WaveOut.GetPosition();
+        public PlaybackState PlaybackState => WaveOut.PlaybackState;
         public float Volume
         {
-            get => _waveOut.Volume;
-            set => _waveOut.Volume = value;
+            get => WaveOut.Volume;
+            set => WaveOut.Volume = value;
         }
         
         public void Dispose()
         {
-            _waveOut?.Dispose();
+            WaveOut?.Dispose();
             _ffMpegStream?.Dispose();
         }
     }
