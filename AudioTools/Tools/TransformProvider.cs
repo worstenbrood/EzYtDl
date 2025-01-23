8c8d8d24adc5c460b41f19f759ad058ea811b1a5
Tim Horemans <tim.horemans@tenforce.com>
2025-01-23 04:17:23 +0100

    Added initial transform provider

---
+++ AudioTools/Tools/TransformProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace AudioTools.Tools
+{
+    public class TransformProvider : ISampleProvider
+    {
+        private readonly ISampleProvider _baseProvider;
+
+        public TransformProvider(ISampleProvider baseProvider)
+        {
+            _baseProvider = baseProvider;
+        }
+
+        public int Read(float[] buffer, int offset, int count)
+        {
+            var result = _baseProvider.Read(buffer, offset, count);
+            return result;
+        }
+
+        public WaveFormat WaveFormat => _baseProvider.WaveFormat;
+    }
+}
