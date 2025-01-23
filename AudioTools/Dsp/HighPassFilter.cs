b98a96dccd97de1cf12eb29aab9d338961cc6971
Tim Horemans <tim.horemans@tenforce.com>
2025-01-23 05:54:56 +0100

    Added hihpass filter

---
+++ AudioTools/Dsp/HighPassFilter.cs
@@ -0,0 +1,27 @@
+﻿using AudioTools.Tools;
+using NAudio.Dsp;
+
+namespace AudioTools.Dsp
+{
+    public class HighPassFilter : IDsp
+    {
+        private volatile BiQuadFilter _filter;
+        private readonly int _sampleRate;
+
+        public HighPassFilter(int sampleRate, float cutoffFrequency, float q)
+        {
+            _sampleRate = sampleRate;
+            _filter = BiQuadFilter.HighPassFilter(_sampleRate, cutoffFrequency, q);
+        }
+
+        public void SetParameters(float cutoffFrequency, float q)
+        {
+            _filter.SetHighPassFilter(_sampleRate, cutoffFrequency, q);
+        }
+
+        public float Transform(float sample)
+        {
+            return _filter.Transform(sample);
+        }
+    }
+}
